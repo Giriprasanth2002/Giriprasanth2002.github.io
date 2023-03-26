bb6b4399e24aaf0d5121e6f3fa3cfe98cd12d5ec
Giriprasanth.S <100854500+Giriprasanth2002@users.noreply.github.com>
2023-03-26 13:44:54 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,25 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$email_subject = $_POST['subject'];
+$message = $_POST['message'];
+$email_from = 'giriprasanth@gmail.com';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'giriprasanth777@gmail.com';
+$email_body = "You have received a new message from the user $name.\n".
+  "You have received a new message from the mail $visitor_email.\n".
+  "and the subject: $email_subject.\n".
+  "Here is the message:\n $message";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$to = "giriprasanth@gmail.com";
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /* 
-  $contact->smtp = array(
-    'host' => 'giriprasanth777@gmail.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );*/
- 
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+// Use a different function name to avoid conflicts with the built-in mail() function
+$custom_mail = mail($to, $email_subject, $email_body, $headers);
+if ($custom_mail) {
+  echo "Your email has been sent!";
+} else {
+  echo "There was a problem sending your email.";
+}
 ?>
